4a5b8cd0e5f8c990e71877f1ff2443c766c10c3e
barocava <brunodavid1992@gmail.com>
2024-05-14 14:41:52 -0400

    Se instala handlebars y websocket

--- src/server.js
+++ src/server.js
@@ -2,11 +2,18 @@ import express from "express";
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import handlebars from "express-handlebars";
+import { __dirname } from "./utils.js";
+import { Server } from 'socket.io'
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts/', cartsRouter);
